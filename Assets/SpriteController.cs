e1f2656d9013f1a512ea05fcfa6a894a71656e18
yutarofruta <furutaro123@gmail.com>
2018-07-12 17:04:19 +0900

    ElasticityPracticeプロジェクト
    2dオブジェクトが指のタッチとマウスの操作に合わせて動くようになった。
    ただ、指でのマルチタッチに反応してしまい、２本目の指が画面に触れるとオブジェクトの位置が変な位置へ移動してしまう。

--- Assets/SpriteController.cs
+++ Assets/SpriteController.cs
@@ -18,10 +18,21 @@ public class SpriteController : MonoBehaviour {
 
     public void OnDrag() {
 
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition.z = 10f;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        transform.position = mousePosition;
+        if(Input.touchCount == 1) {
+            Touch touch = Input.GetTouch(0);
+            Vector3 vec = touch.position;
+            vec.z = 10f;
+            vec = Camera.main.ScreenToWorldPoint(vec);
+            transform.position = vec;
+        }
+
+        if(Input.GetMouseButton(0)) {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 10f;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = mousePosition;
+        }
+
         gameObject.GetComponent<SpringJoint2D>().enabled = false;
 
     }
