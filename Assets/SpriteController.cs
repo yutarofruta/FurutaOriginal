7e36884e782014ceee10bb16891f3fc868ecc16c
yutarofruta <furutaro123@gmail.com>
2018-07-12 15:47:21 +0900

    ElasticityPracticeプロジェクト
    2Dオブジェクトを画面上で指で動かせるようにするために、EventSystemを使用した。
    これでオブジェクトをタッチしたときにだけ動かせるようになった。
    ただ、タッチしてドラッグしていないときにオブジェクトが小刻みに震えてしまう

--- Assets/SpriteController.cs
+++ Assets/SpriteController.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class SpriteController : MonoBehaviour {
 
     public GameObject goal;
-
-
+    
 	// Use this for initialization
 	void Start () {
 		
@@ -14,38 +13,15 @@ public class SpriteController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.touchCount > 0) {
 
-            Touch touch = Input.GetTouch(0);
-            Vector3 vec = touch.position;
-            vec.z = 10f;
-            vec = Camera.main.ScreenToWorldPoint(vec);
-            transform.position = vec;
-
-            float distance = Vector3.Distance(vec, goal.transform.position);
-            if (distance <= 1f) {
-                transform.position = goal.transform.position;
-
-            }
-
-
-        }
-
-        if (Input.GetMouseButton(0)) {
-
-            Vector3 mousePosition = Input.mousePosition;
-            mousePosition.z = 10f;
-            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            transform.position = mousePosition;
-
-            float distance = Vector3.Distance(mousePosition, goal.transform.position);
-            if (distance <= 1f) {
-                transform.position = goal.transform.position;
-
-            }
-
-
-        }
     }
 
+    public void OnDrag() {
+
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 10f;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = mousePosition;
+
+    }
 }
