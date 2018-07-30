3daac9b87eaf641246b7cb9e8a8b00cd396955bb
yutarofruta <furutaro123@gmail.com>
2018-07-30 17:11:06 +0900

    FurutaOriginal
    GameMenuを作成しメニューからゲームへの遷移を作った。
    Levelごとに難易度を変える操作はまだ行っていない。

---
+++ Assets/Script/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject popUp;
+    private bool isPopUpActive = false;
+    private string game;
+
+    private void Start() {
+        popUp.SetActive(false);
+    }
+
+    public void LoadGame(string level) {
+
+        SceneManager.LoadScene(game);
+    }
+
+    public void PopUpSetActice(string game) {
+
+        this.game = game;
+
+        isPopUpActive = !isPopUpActive;
+        popUp.SetActive(isPopUpActive);
+    }
+}
